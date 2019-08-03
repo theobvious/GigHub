769bd768a00e80aa368842bb49f48c291fef6288
Theobvious <olga.lempert@gmail.com>
2019-08-03 13:46:44 +0300

    moved datetime conversion to form view model

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,7 +1,6 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
 using Microsoft.AspNet.Identity;
-using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -34,8 +33,8 @@ namespace GigHub.Controllers
           
             var gig = new Gig
             {
-                ArtistId = User.Identity.GetUserId(),s
-                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                ArtistId = User.Identity.GetUserId(),
+                DateTime = viewModel.DateTime,
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
             };
