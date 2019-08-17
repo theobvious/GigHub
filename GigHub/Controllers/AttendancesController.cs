9e199c8a87eedc1b56734daafd010faa45281d5f
Theobvious <olga.lempert@gmail.com>
2019-08-17 20:01:39 +0300

    implemented gig attendance

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,38 @@
+﻿using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+using GigHub.DTOs;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto dto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Attendances.Any(a => a.AttendeeId == userId && a.GigId == dto.GigId))
+                return BadRequest("Attendance already exists.");
+
+            var attendance = new Attendance
+            {
+                GigId = dto.GigId,
+                AttendeeId = userId
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
--- GigHub/Models/ApplicationDbContext.cs
+++ GigHub/Models/ApplicationDbContext.cs
@@ -10,6 +10,8 @@ namespace GigHub.Models
 
         public DbSet<Genre> Genres { get; set; }
 
+        public DbSet<Attendance> Attendances { get; set; }
+
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
@@ -18,6 +20,16 @@ namespace GigHub.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
